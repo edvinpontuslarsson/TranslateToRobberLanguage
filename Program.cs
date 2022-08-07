a17c137490415c94459c44094ce1ba8eb8b7ebab
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-08-07 14:48:50 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TranslateToRobberLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input and get it repeated: ");
+                string input = Console.ReadLine();
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Program that translates input to the robber language
+https://en.wikipedia.org/wiki/R%C3%B6varspr%C3%A5ket
+
+For example "stubborn" will be translated to "sostotubobboborornon".
